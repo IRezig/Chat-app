101141af9f3e424da1441a0fefed5fa31c76706b
rezig <rezig.ibtissam@gmail.com>
2021-09-27 10:16:02 +0200

    Add user functions

--- client/src/components/Chat/Chat.jsx
+++ client/src/components/Chat/Chat.jsx
@@ -1,12 +1,14 @@
 import React, {useState, useEffect} from 'react'
 import queryString from 'query-string'
 import io from 'socket.io-client'
-
+import './Chat.css'
 let socket;
 
 export default function Chat({location}) {
   const [name, setName] = useState('')
   const [room, setRoom] = useState('')
+  const [message, setMessage] = useState('')
+  const [messages, setMessages] = useState([])
   const ENDPOINT = 'localhost:5000'
 
   useEffect(() => {
@@ -24,12 +26,35 @@ export default function Chat({location}) {
 
   }, [ENDPOINT, location.search])
 
+  useEffect(() => {
+    socket.on('message', (message) => {
+      setMessages([...messages, message])
+    })
+  }, [messages])
+
+  const sendMessage = (e) => {
+    e.preventDefault()
+    if(message) {
+      socket.emit('sendMessage', message, () => setMessage('') )
+    }
+  }
+
+  console.log(message, messages)
+
   return (
     <div className="outerContainer">
       <div className="container">
-          {/* <InfoBar room={room} />
-          <Messages messages={messages} name={name} />
-          <Input message={message} setMessage={setMessage} sendMessage={sendMessage} /> */}
+          {/* <InfoBar room={room} /> */}
+          {/* <Messages messages={messages} name={name} /> */}
+          {/* <Input 
+            message={message} 
+            setMessage={setMessage} 
+            sendMessage={sendMessage} /> */}
+          <input 
+            value='message' 
+            onChange={(e)=> setMessage(e.target.value) }
+            onKeyPress={(e) => e.key === 'Enter' ? sendMessage(e) : null }
+          />
       </div>
       {/* <TextContainer users={users}/> */}
     </div>
--- server/index.js
+++ server/index.js
@@ -4,6 +4,8 @@ const http = require('http')
 
 const PORT = process.env.PORT || 5000
 const router = require('./router')
+const { addUser, removeUser, getUsersInRoom, getUser } = require('./users.js')
+
 
 const app = express()
 const server = http.createServer(app)
@@ -17,9 +19,22 @@ const io = socketio(server , {
 io.on('connection', (socket) => {
   console.log('we have a new connection')
   socket.on('join', ({ name, room }, callback) => {
-    console.log(name, room)
+    const { error, user } = addUser({ id: socket.id, name, room })
+    if(error) return callback(error)
+    
+    socket.emit('message', { user: 'admin', text: `${user.name}, welcome to the room ${user.room}` })
+    socket.broadcast.to(user.room).emit('message', {user: 'admin', text: `${user.name} has joined!` })
+    socket.join(user.room)
+    callback()
+
+  })
+
+  socket.on('sendMessage', ({message, callback}) => {
+    const user = getUser(socket.id)
+    io.to(user.room).emit('messgae', { user: user.name, text: message } )
     callback()
   })
+
   socket.on('disconnect', () => {
     console.log('User has left!')
   })
